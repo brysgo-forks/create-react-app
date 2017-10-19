8313130df904a4c2a9e8edd9f3f7babcb46b58ee
Bryan Goldstein <goldstein.bryan@bcg.com>
2017-10-18 21:36:09 -0400

    make tests pass

--- packages/react-scripts/scripts/prettier.js
+++ packages/react-scripts/scripts/prettier.js
@@ -1,3 +1,4 @@
+'use strict';
 const runAll = require("lint-staged/src/runAll");
 const getConfig = require('lint-staged/src/getConfig').getConfig
 const validateConfig = require('lint-staged/src/getConfig').validateConfig
